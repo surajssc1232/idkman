c573e7c9c35b00f65b83b86bce4f53194df4e6ed
suraj singh <surajsinghchauhan8923@gmail.com>
2025-06-25 04:56:56 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Footer>
    
    with the

--- src/components/devcard/footer.tsx
+++ src/components/devcard/footer.tsx
@@ -1,8 +1,7 @@
 export function Footer() {
   return (
     <footer className="w-full p-4 md:p-8">
-      <div className="mx-auto flex max-w-7xl items-center justify-between text-sm text-foreground/60">
-        <p>SURAJ CHAUHAN</p>
+      <div className="mx-auto flex max-w-7xl items-center justify-end text-sm text-foreground/60">
         <div className="flex items-center gap-2">
           <span className="relative flex h-2 w-2">
             <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
